6fa7027f0c6ce73bfaf8a7a58993ff80e71438e8
Giulio Caprino <giulio.caprino@dataglide.co>
2022-10-26 20:03:46 +0100

    v1.0

--- Lucca Comics And Games 2022.service.worker.js
+++ Lucca Comics And Games 2022.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1666555113|1647711726";
+const CACHE_VERSION = "1666810688|379716131";
 const CACHE_PREFIX = "Lucca Comics And-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "Lucca Comics And Games 2022.offline.html";
